8c943f469176f163dc81da62400068eebf5c7502
Helge Waastad <helge.waastad@datametrix.no>
2016-01-30 11:40:26 +0100

    fix accordion add primefaces showcase styles
    TODO: fix panel

--- aurelia-primefaces/elements/panel.js
+++ aurelia-primefaces/elements/panel.js
@@ -1,52 +1,77 @@
 import {inject, customElement,bindable} from 'aurelia-framework';
 
+
 @customElement('p-panel')
 @inject(Element)
 export class PanelComponent {
   @bindable header: String;
   @bindable toggleable: Boolean;
-  @bindable toggleDuration: any;
-  @bindable toggleOrientation: any;
+  @bindable toggleduration: any;
+  @bindable toggleorientation: any;
   @bindable closable: Boolean;
-  @bindable closeDuration: any;
+  @bindable closeduration: any;
   @bindable collapsed: boolean;
-  @bindable onBeforeCollapse;
-  @bindable onAfterCollapse;
-  @bindable onBeforeExpand;
-  @bindable onAfterExpand;
-  @bindable onBeforeClose;
-  @bindable onAfterClose;
+  @bindable onbeforecollapse;
+  @bindable onaftercollapse;
+  @bindable onbeforeexpand;
+  @bindable onafterexpand;
+  @bindable onbeforeclose;
+  @bindable onafterclose;
   initialized: boolean;
 
   constructor(element){
+    console.log('panel contruct');
     this.element=element;
     this.initialized = false;
   }
 
   attached(){
+    console.log('panel attached');
     console.log('closable: ' + this.closable + ' toggleable: ' + this.toggleable);
-    $(this.element).puipanel({
+  //  console.dir(this.element.children[0]);
+    $(this.element.children[0]).puipanel({
       title: this.header,
       toggleable: this.toggleable,
-      toggleDuration: this.toggleDuration,
-      toggleOrientation: this.toggleOrientation,
+      toggleDuration: this.toggleduration,
+      toggleOrientation: this.toggleorientation,
       collapsed: this.collapsed,
       closable:this.closable,
-      closeDuration: this.closeDuration,
-      /*beforeCollapse: this.onBeforeCollapse ? (event: Event) => { this.onBeforeCollapse(event); } : null,
-      afterCollapse: this.onAfterCollapse ? (event: Event) => { this.onAfterCollapse(event); } : null,
-      beforeExpand: this.onBeforeExpand ? (event: Event) => { this.onBeforeExpand(event); } : null,
-      afterExpand: this.onAfterExpand ? (event: Event) => { this.onAfterExpand(event); } : null,
-      beforeClose: this.onBeforeClose ? (event: Event) => { this.onBeforeClose(event); } : null,
-      afterClose: this.onAfterClose ? (event: Event) => { this.onAfterClose(event); } : null,*/
+      closeDuration: this.closeduration,
+    /*beforeCollapse: this.onbeforecollapse ? (event: Event) => { this.onbeforecollapse(event); } : null,
+      afterCollapse: this.onaftercollapse ? (event: Event) => { this.onaftercollapse(event); } : null,
+      beforeExpand: this.onbeforeexpand ? (event: Event) => { this.onbeforeexpand(event); } : null,
+      afterExpand: this.onafterexpand ? (event: Event) => { this.onafterexpand(event); } : null,
+      beforeClose: this.onbeforeclose ? (event: Event) => { this.onbeforeclose(event); } : null,
+      afterClose: this.onafterclose ? (event: Event) => { this.onafterclose(event); } : null,*/
       enhanced: true
     });
     this.initialized = true;
   }
 
   detached(){
-    $(this.element).puipanel('destroy');
+    console.log('panel detached');
+    $(this.element.children[0]).puipanel('destroy');
     this.initialized = false;
+  }
+
+  //Logging
+  bind() {
+    console.log('panel bind');
+  }
+
+  activated() {
+    console.log('panel activated');
+  }
+  created() {
+    console.log('panel created');
+  }
+
+  activate() {
+    console.log('panel activate');
+  }
+
+  canActivate() {
+    console.log('panel canActivate');
   }
 
 }
