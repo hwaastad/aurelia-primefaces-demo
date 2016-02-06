326b441581054b97f258d2337a8633bc92efe107
Helge Waastad <helge.waastad@datametrix.no>
2016-02-06 17:02:06 +0100

    radiobutton implemented

--- aurelia-primefaces/elements/radiobutton.js
+++ aurelia-primefaces/elements/radiobutton.js
@@ -1,36 +1,28 @@
-import {inject, customElement,bindable} from 'aurelia-framework';
+import {inject, customElement, ObserverLocator, bindable, computedFrom} from 'aurelia-framework';
 
 @customElement('p-radio')
 @inject(Element)
 export class RadioButtonComponent {
+  element = undefined;
+  @bindable model: any;
   @bindable value: any;
   @bindable name: string;
-  @bindable disabled: boolean;
-  @bindable model: any;
-  @bindable click;
 
-  constructor(element){
-    this.element=element;
+  @bindable checked;
+
+  constructor(element) {
+    this.element = element;
   }
 
-  onClick(input) {
-    input.checked = true;
-    console.dir(input.checked);
-    //this.model = input.;
+  onclick(input) {
+    this.model = input.value;
   }
 
-  modelChanged(){
-    console.log('model has changed...');
-  }
-
-  isChecked(){
-    console.log('checking ' + this.value + '=' + this.model);
-    if(this.value===this.model){
-      return true;
+  modelChanged() {
+    if (this.value === this.model) {
+      this.checked = true;
     } else {
-      return false;
+      this.checked = false;
     }
-    //returning (this.value == this.model);
-    //return this.value == this.model;
   }
 }
