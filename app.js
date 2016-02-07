ad7a7d18fd31a86db537de263e27e3a985f10a3f
Helge Waastad <helge.waastad@datametrix.no>
2016-02-07 21:22:21 +0100

    start implementing fieldset....

--- app.js
+++ app.js
@@ -33,7 +33,8 @@ export class App {
       { route: 'spinner',         name: 'spinner',        moduleId: 'pages/components/spinner',        nav: true, title: 'Spinner' },
       { route: 'toggelbutton',         name: 'toggelbutton',        moduleId: 'pages/components/toggelbutton',        nav: true, title: 'ToggelButton' },
       { route: 'inputswitch',         name: 'inputswitch',        moduleId: 'pages/components/inputswitch',        nav: true, title: 'InputSwitch' },
-      { route: 'progressbar',         name: 'progressbar',        moduleId: 'pages/components/progressbar',        nav: true, title: 'ProgressBar' }
+      { route: 'progressbar',         name: 'progressbar',        moduleId: 'pages/components/progressbar',        nav: true, title: 'ProgressBar' },
+      { route: 'fieldset',         name: 'fieldset',        moduleId: 'pages/components/fieldset',        nav: true, title: 'FieldSet' }
     ]);
 
     this.router = router;
---
+++ aurelia-primefaces/elements/fieldset.js
@@ -0,0 +1,47 @@
+import {inject, customElement, bindable} from 'aurelia-framework';
+
+@customElement('p-fieldset')
+@inject(Element)
+export class FieldSetConmponent {
+    @bindable legend = undefined;
+    @bindable toggleable = undefined;
+    @bindable toggleDuration = undefined;
+    @bindable collapsed = undefined;
+    @bindable onBeforeToggle;
+    @bindable onAfterToggle;
+
+    initialized;
+
+    constructor(element) {
+        this.element = element;
+        this.initialized = false;
+    }
+
+    attached() {
+        $(this.element.children[0]).puifieldset({
+            toggleable: this.toggleable,
+            toggleDuration: this.toggleDuration,
+            collapsed: this.collapsed,
+            beforeToggle: this.onBeforeToggle ? (event: Event, collapsed: boolean) => {
+                this.onBeforeToggle.next({ 'originalEvent': event, 'collapsed': collapsed });
+            } : null,
+            afterToggle: this.onAfterToggle ? (event: Event, collapsed: boolean) => {
+                this.onAfterToggle.next({ 'originalEvent': event, 'collapsed': collapsed });
+            } : null,
+            enhanced: true
+        });
+        this.initialized = true;
+    }
+
+    propertyChanged(property, newValue, oldValue) {
+        if (this.initialized) {
+            $(this.element.children[0]).puifieldset('option', property, newValue);
+        }
+    }
+
+    detached() {
+        $(this.element.children[0]).puifieldset('destroy');
+        this.initialized = false;
+    }
+
+}
