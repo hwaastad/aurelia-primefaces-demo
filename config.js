b980f8311791d7dba8b5e95d3a66eab483f964ce
Helge Waastad <helge.waastad@datametrix.no>
2016-03-02 08:12:23 +0100

    update libs add fonts

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ System.config({
     "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.4",
     "aurelia-http-client": "npm:aurelia-http-client@1.0.0-beta.1.1.2",
     "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.0.0",
-    "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.2",
+    "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.3",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
     "chart.js": "npm:chart.js@1.0.2",
@@ -30,7 +30,6 @@ System.config({
     "jquery": "npm:jquery@2.2.1",
     "jquery-ui": "github:components/jqueryui@1.11.4",
     "primeui": "bower:primeui@4.1.1",
-    "redpelicans/aurelia-material": "github:redpelicans/aurelia-material@master",
     "bower:jqueryui@1.11.4": {
       "jquery": "bower:jquery@2.2.1"
     },
@@ -54,11 +53,6 @@ System.config({
     "github:jspm/nodelibs-util@0.1.0": {
       "util": "npm:util@0.10.3"
     },
-    "github:redpelicans/aurelia-material@master": {
-      "google/material-design-lite": "github:google/material-design-lite@1.1.1",
-      "jquery": "github:components/jquery@2.2.1",
-      "lodash": "npm:lodash@3.10.1"
-    },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
@@ -77,7 +71,7 @@ System.config({
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
       "aurelia-pal-browser": "npm:aurelia-pal-browser@1.0.0-beta.1.1.4",
       "aurelia-polyfills": "npm:aurelia-polyfills@1.0.0-beta.1.0.0",
-      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.2",
+      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.3",
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.2",
       "aurelia-templating-binding": "npm:aurelia-templating-binding@1.0.0-beta.1.1.2",
       "aurelia-templating-resources": "npm:aurelia-templating-resources@1.0.0-beta.1.1.2",
@@ -135,7 +129,7 @@ System.config({
     "npm:aurelia-route-recognizer@1.0.0-beta.1.1.2": {
       "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.1.1"
     },
-    "npm:aurelia-router@1.0.0-beta.1.1.2": {
+    "npm:aurelia-router@1.0.0-beta.1.1.3": {
       "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.0.0-beta.1.1.4",
       "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0-beta.1.1.1",
       "aurelia-history": "npm:aurelia-history@1.0.0-beta.1.1.1",
@@ -167,7 +161,7 @@ System.config({
       "aurelia-metadata": "npm:aurelia-metadata@1.0.0-beta.1.1.5",
       "aurelia-pal": "npm:aurelia-pal@1.0.0-beta.1.1.1",
       "aurelia-path": "npm:aurelia-path@1.0.0-beta.1.1.1",
-      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.2",
+      "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.3",
       "aurelia-templating": "npm:aurelia-templating@1.0.0-beta.1.1.2"
     },
     "npm:aurelia-templating@1.0.0-beta.1.1.2": {
@@ -196,9 +190,6 @@ System.config({
     },
     "npm:inherits@2.0.1": {
       "util": "github:jspm/nodelibs-util@0.1.0"
-    },
-    "npm:lodash@3.10.1": {
-      "process": "github:jspm/nodelibs-process@0.1.2"
     },
     "npm:path-browserify@0.0.0": {
       "process": "github:jspm/nodelibs-process@0.1.2"
