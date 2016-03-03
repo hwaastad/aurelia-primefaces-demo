4c0118b7c1211f2c59ac9324479cc2a2cf37d0d0
Helge Waastad <helge.waastad@datametrix.no>
2016-03-03 15:42:18 +0100

    start checking reordering

--- aurelia-primefaces/elements/datatable.js
+++ aurelia-primefaces/elements/datatable.js
@@ -167,13 +167,6 @@ export class DataTableComponent {
         }
     }
 
-    @computedFrom('selection')
-    get selected() {
-        console.log('selected check...');
-        return this.selected;
-    }
-
-
     selectionChanged(newVal, oldVal) {
         console.log('selection has changed: ' + oldVal + ' => ' + newVal);
         this.selection = newVal;
@@ -243,13 +236,6 @@ export class DataTableComponent {
                     this.animator.removeClass(el, 'ui-state-highlight');
                 }
             }
-            /* for (var index = 0; index < this.value.length; index++) {
-                 var element = this.value[index];
-                 if (containsObject(element, this.selection)) {
-                     let el = this.element.querySelectorAll('tbody tr')[index];
-                     this.animator.addClass(el, 'ui-state-highlight');
-                 }
-             }*/
         }
     }
 
@@ -444,10 +430,10 @@ export class DataTableComponent {
 
     initColumnReordering() {
         $(this.element.children[0]).puicolreorder({
-            colReorder: (event: Event, ui: PrimeUI.ColReorderEventParams) => {
+            colReorder: (event: Event, ui: eventParams) => {
                 //right
                 if (ui.dropSide > 0) {
-                    this.columns.splice(ui.dropIndex + 1, 0, this.columns.splice(ui.dragIndex, 1)[0]);
+                    this.columns.splice(+ui.dropIndex + +1, 0, this.columns.splice(ui.dragIndex, 1)[0]);
                 }
                 //left
                 else {
